85919159af873c7a72cc717c622fafc22525731a
Lưu Minh Hoàng <ceriagame@gmail.com>
2020-09-24 13:44:39 +0700

    Initialized TourDuLich-GUI DevExpress WinForms MVVM

---
+++ TourDuLich/TourDuLich-GUI/MainView.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace TourDuLich_GUI
+{
+    partial class MainView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mvvmContext1 = new DevExpress.Utils.MVVM.MVVMContext(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.mvvmContext1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mvvmContext1
+            // 
+            this.mvvmContext1.ContainerControl = this;
+            this.mvvmContext1.ViewModelType = typeof(TourDuLich_GUI.MainViewModel);
+            // 
+            // MainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 278);
+            this.Name = "MainView";
+            this.Text = "MainView";
+            ((System.ComponentModel.ISupportInitialize)(this.mvvmContext1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.Utils.MVVM.MVVMContext mvvmContext1;
+    }
+}
+
---
+++ TourDuLich/TourDuLich-GUI/MainView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TourDuLich_GUI
+{
+    public partial class MainView : DevExpress.XtraEditors.XtraForm
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            if (!mvvmContext1.IsDesignMode)
+                InitializeBindings();
+        }
+
+        void InitializeBindings()
+        {
+            var fluent = mvvmContext1.OfType<MainViewModel>();
+        }
+    }
+}
