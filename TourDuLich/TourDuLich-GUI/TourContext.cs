19e870b1a760aa8e89878d6bbb37fe06cc7d8d85
Lưu Minh Hoàng <ceriagame@gmail.com>
2020-09-29 13:58:33 +0700

    Added db contexts

--- TourDuLich/TourDuLich-GUI/TourContext.cs
+++ TourDuLich/TourDuLich-GUI/TourContext.cs
@@ -14,7 +14,7 @@ namespace TourDuLich_GUI
         // If you wish to target a different database and/or database provider, modify the 'Model1' 
         // connection string in the application configuration file.
         public Model1()
-            : base("TourContext")
+            : base("TDLMinhHoangContext")
         {
         }
 
