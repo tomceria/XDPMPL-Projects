2ceb10541cd4aea576092edb5848caea55f5350b
Lưu Minh Hoàng <ceriagame@gmail.com>
2020-10-30 15:12:01 +0700

    Views/CongViet/Edit: Adjustment

--- TodoList/Controllers/CongViecController.cs
+++ TodoList/Controllers/CongViecController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TodoList.Data;
 using TodoList.Models;
 
@@ -15,11 +16,11 @@ namespace TodoList.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.DSCongViec.ToListAsync());
         }
-
+        
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)    // TODO: Check with database
@@ -30,8 +31,39 @@ namespace TodoList.Controllers
             CongViec congViec = new CongViec();
             congViec.ID = (int) id;
             congViec.Name = "Hello there!!!";
+            congViec.StartDate = DateTime.Now;
 
             return View(congViec);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,StartDate,EndDate,TrangThai,Privacy")] CongViec congViec)
+        {
+            if (id != congViec.ID)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid) return View(congViec);
+            try
+            {
+                _context.Update(congViec);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return NotFound();
+                // if (!CongViecExists(congViec.ID))
+                // {
+                //     return NotFound();
+                // }
+                // else
+                // {
+                //     throw;
+                // }
+            }
+            return RedirectToAction("Index");
         }
     }
 }
