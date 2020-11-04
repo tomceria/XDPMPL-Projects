2c53542fc08af75228a45eaff87cb9e194d12301
JaySGH <huysgh94@gmail.com>
2020-11-04 14:21:36 +0700

    first business report logic

---
+++ TourDuLich/TourDuLich-GUI/BUS/Report/TourBusinessReport.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using TourDuLich_GUI.DAL;
+
+namespace TourDuLich_GUI.BUS.Report {
+    public partial class TourBusinessReport {
+        public List<TourBusinessReport> getTourBusinessReport(DateTime startDate, DateTime endDate) {
+            List<Tour> tours = TourDAL.GetAll();
+            List<TourBusinessReport> reports = new List<TourBusinessReport>();
+
+            // thống kê theo từng tour
+            foreach (Tour tour in tours) {
+                TourBusinessReport report = new TourBusinessReport();
+
+                foreach (TourGroup tourGroup in tour.TourGroups) {
+                    report.TourCostPerCostType = new Dictionary<CostType, long>();
+                    report.Sales += tourGroup.PriceGroup;
+                    report.CustomerCount += tourGroup.TourGroupDetails.Count;
+                    
+                    foreach (TourGroupCost tourGroupCost in tourGroup.TourGroupCosts) {
+                        report.TotalCost += tourGroupCost.Value;
+                        report.TourCostPerCostType.Add(tourGroupCost.CostType, tourGroupCost.Value);
+                    }
+                }
+
+                report.TourGroupCount = tour.TourGroups.Count;
+            }
+            
+            return reports;
+        }
+    }
+}
---
+++ TourDuLich/TourDuLich-GUI/Models/Transient/TourBusinessReport.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TourDuLich_GUI.BUS.Report {
+    public partial class TourBusinessReport {
+        Tour Tour;
+        long Sales;
+        int CustomerCount;
+        int TourGroupCount;
+        long TotalCost;
+        Dictionary<CostType, long> TourCostPerCostType;
+    }
+}
