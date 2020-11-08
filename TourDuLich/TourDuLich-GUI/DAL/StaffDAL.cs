c57aa5f470535e8e1cc8d7c5d40023517277a55f
JaySGH <huysgh94@gmail.com>
2020-11-08 23:00:53 +0700

    staff activities report

--- TourDuLich/TourDuLich-GUI/DAL/StaffDAL.cs
+++ TourDuLich/TourDuLich-GUI/DAL/StaffDAL.cs
@@ -1,38 +1,42 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using TourDuLich_GUI.BUS;
 
-namespace TourDuLich_GUI.DAL
-{
-    class StaffDAL
-    {
+namespace TourDuLich_GUI.DAL {
+    class StaffDAL {
         static TourContext _ctx = new TourContext();
 
-        public StaffDAL()
-        {
+        public StaffDAL() {
             _ctx = new TourContext();
         }
 
-        public static List<Staff> GetAll()
-        {
+        public static List<Staff> GetAll() {
             var staffs = _ctx.Staffs.ToList();
 
             return staffs;
         }
-        public static Staff GetOne(int id)
-        {
+
+        public static List<Staff> GetAll(DateTime startDate, DateTime endDate) {
+            // lọc ra những tour group có ngày khởi hành trong khoảng thời gian tìm kiếm
+            var result = _ctx.Staffs.ToList().ConvertAll(staff => new Staff(staff) {
+                TourGroupStaffs = staff.TourGroupStaffs.Where(tg => tg.TourGroup.DateStart >= startDate && tg.TourGroup.DateStart <= endDate).ToList()
+            });
+
+            return result;
+        }
+
+        public static Staff GetOne(int id) {
             // if found => return staff, else return null
             return _ctx.Staffs.Find(id);
         }
 
-        public static void CreateOne(Staff staff)
-        {
+        public static void CreateOne(Staff staff) {
             _ctx.Staffs.Add(staff);
             _ctx.SaveChanges();
         }
 
-        public static Staff UpdateOne(Staff staff)
-        {
+        public static Staff UpdateOne(Staff staff) {
             // get by id
             var staffToUpdate = _ctx.Staffs.Find(staff.ID);
 
@@ -45,8 +49,7 @@ namespace TourDuLich_GUI.DAL
             return staffToUpdate;
         }
 
-        public static void DeleteOne(int id)
-        {
+        public static void DeleteOne(int id) {
             var staff = _ctx.Staffs.Find(id);
             _ctx.Staffs.Remove(staff);
             _ctx.SaveChanges();
---
+++ TourDuLich/TourDuLich-GUI/Models/Transient/StaffActivitiesReport.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TourDuLich_GUI.BUS.Report {
+    public partial class StaffActivitiesReport {
+        public Staff Staff { get; set; }
+        public int TourGroupCount { get; set; }
+        public ICollection<TourGroup> TourGroups { get; set; }
+
+        public StaffActivitiesReport() {
+            Staff = new Staff();
+            TourGroupCount = 0;
+        }
+    }
+}
