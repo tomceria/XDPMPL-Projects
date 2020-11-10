0abb916bb6eea2b2177b3fcc14f1dddfc69572b3
Lưu Minh Hoàng <ceriagame@gmail.com>
2020-11-10 11:13:51 +0700

    Updated SeedData

--- TourDuLich/TourDuLich-GUI/DAL/TourInitializer.cs
+++ TourDuLich/TourDuLich-GUI/DAL/TourInitializer.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using TourDuLich_GUI.BUS;
 using System.Data.Entity;
+using TourDuLich_GUI.BUS;
 
-namespace TourDuLich_GUI.DAL {
-    class TourInitializer : DropCreateDatabaseIfModelChanges<TourContext> {
-        protected override void Seed(TourContext context) {
-            IList<TourType> tourTypes = new List<TourType>();
-            tourTypes.Add(new TourType() { Name = "Du lịch di động" });
-            tourTypes.Add(new TourType() { Name = "Du lịch kết hợp nghề nghiệp" });
-            tourTypes.Add(new TourType() { Name = "Du lịch xã hội và gia đình" });
-
-            context.TourTypes.AddRange(tourTypes);
-            context.SaveChanges();
+namespace TourDuLich_GUI.DAL
+{
+    class TourInitializer : DropCreateDatabaseIfModelChanges<TourContext>
+    {
+        protected override void Seed(TourContext context)
+        {
         }
     }
 }
