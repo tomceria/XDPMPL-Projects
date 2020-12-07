942c211bc37450dcd3b9ee5346a4ac606d9f9e7e
hieuit123 <43605814+hieuit123@users.noreply.github.com>
2020-12-07 23:08:14 +0700

    View/Staff/Me: Create view staff info

--- TodoList/Controllers/StaffController.cs
+++ TodoList/Controllers/StaffController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TodoList.Data;
@@ -9,15 +10,35 @@ namespace TodoList.Controllers
     public class StaffController : Controller
     {
         private readonly IStaffService _staffService;
+        private readonly IAccountService _accountService;
 
-        public StaffController(IStaffService staffService)
+        public StaffController(IStaffService staffService, IAccountService accountService)
         {
             _staffService = staffService;
+            _accountService = accountService;
         }
 
         public IActionResult Me()
         {
-            return View();
+            Console.WriteLine("Da chuyen ");
+            var viewModel = _staffService.GetCurrentUser(User);
+            return View(viewModel);
+        }
+
+        public async Task<IActionResult> Update(
+            [Bind("UserName,Email,StaffId,Staff")]
+            ApplicationUser viewModel
+        )
+        {
+            ApplicationUser updatedApplicationUser = _staffService.GetCurrentUser(User);
+            //update staff info
+            updatedApplicationUser.Email = viewModel.Email;
+            updatedApplicationUser.Staff.FirstName = viewModel.Staff.FirstName;
+            updatedApplicationUser.Staff.LastName = viewModel.Staff.LastName;
+
+            await _accountService.Update(updatedApplicationUser);
+
+           return RedirectToAction("Me","Staff");
         }
     }
 }
