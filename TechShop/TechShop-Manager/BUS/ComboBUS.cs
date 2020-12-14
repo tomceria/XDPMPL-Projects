75d73d572eca7d71878fd181cb755432e5e05e16
hieuit123 <43605814+hieuit123@users.noreply.github.com>
2020-12-15 00:44:57 +0700

    Add CRUD for Combo entity

---
+++ TechShop/TechShop-Manager/BUS/ComboBUS.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechShop_Manager.DAL;
+
+namespace TechShop_Manager.BUS
+{
+    public partial class Combo
+    {
+        public static List<Combo> GetAll()
+        {
+            return ComboDAL.GetAll();
+        }
+
+        public static Combo GetOne(int id)
+        {
+            return ComboDAL.GetOne(id);
+        }
+
+        public static Combo CreateOne(Combo combo)
+        {
+            return ComboDAL.CreateOne(combo);
+        }
+
+        public static void UpdateOne(Combo combo)
+        {
+            ComboDAL.UpdateOne(combo);
+        }
+
+        public static void DeleteOne(int id)
+        {
+            ComboDAL.DeleteOne(id);
+        }
+    }
+}
---
+++ TechShop/TechShop-Manager/DAL/ComboDAL.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using TechShop_Manager.BUS;
+
+namespace TechShop_Manager.DAL
+{
+    public class ComboDAL
+    {
+        static ShopContext _ctx = new ShopContext();
+
+        public ComboDAL()
+        {
+            _ctx = new ShopContext();
+        }
+
+        public static List<Combo> GetAll()
+        {
+            var combos = _ctx.Combos.ToList();
+            return combos;
+        }
+
+        public static Combo GetOne(int id)
+        {
+            return _ctx.Combos.FirstOrDefault(o => o.Id == id);
+        }
+
+        public static Combo CreateOne(Combo combo)
+        {
+            _ctx.Entry(combo).State = EntityState.Added;
+            _ctx.SaveChanges();
+
+            return combo;
+        }
+
+        public static void UpdateOne(Combo combo)
+        {
+            _ctx.Entry(combo).State = EntityState.Modified;
+
+            List<ComboDetail> ComboDetails = combo.ComboDetails.ToList();
+            List<ComboDetail> ogComboDetails = _ctx.ComboDetails.Where(o => o.ComboId == combo.Id).ToList();
+
+            foreach (ComboDetail ogComboDetail in ogComboDetails)
+            {
+                if (ComboDetails.FirstOrDefault(o => o.ComboId == ogComboDetail.ComboId) == null)
+                {
+                    _ctx.Entry(ogComboDetail).State = EntityState.Deleted;
+                }
+            }
+
+            foreach (ComboDetail ComboDetail in ComboDetails)
+            {
+                if (ogComboDetails.FirstOrDefault(o => o.ComboId == ComboDetail.ComboId) == null)
+                {
+                    _ctx.Entry(ComboDetail).State = EntityState.Added;
+                }
+                else
+                {
+                    _ctx.Entry(ComboDetail).State = EntityState.Modified;
+                }
+            }
+
+            _ctx.SaveChanges();
+        }
+
+        public static void DeleteOne(int id)
+        {
+            Combo combo = _ctx.Combos.FirstOrDefault(o => o.Id == id);
+            combo.IsHidden = true;
+            _ctx.SaveChanges();
+        }
+
+    }
+}
