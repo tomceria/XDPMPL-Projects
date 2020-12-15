d4203938f81eded5335dd0f4e921a1228e6e554e
hieuit123 <43605814+hieuit123@users.noreply.github.com>
2020-12-15 13:02:20 +0700

    Update fun CreateOne() convert slug from name and id

--- TechShop/TechShop-Manager/BUS/ComboBUS.cs
+++ TechShop/TechShop-Manager/BUS/ComboBUS.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TechShop_Manager.DAL;
 
@@ -21,6 +22,7 @@ namespace TechShop_Manager.BUS
 
         public static Combo CreateOne(Combo combo)
         {
+            combo.Slug = ConvertToSlug(combo.Name, combo.Id);
             return ComboDAL.CreateOne(combo);
         }
 
@@ -32,6 +34,20 @@ namespace TechShop_Manager.BUS
         public static void DeleteOne(int id)
         {
             ComboDAL.DeleteOne(id);
+        }
+
+        public static string ConvertToUnSign(string s)
+        {
+            Regex regex = new Regex("\\p{IsCombiningDiacriticalMarks}+");
+            string temp = s.Normalize(NormalizationForm.FormD);
+            return regex.Replace(temp, String.Empty).Replace('\u0111', 'd').Replace('\u0110', 'D');
+        }
+
+        public static string ConvertToSlug(string name, int id)
+        {
+            string unsignName = ConvertToUnSign(name);
+            unsignName = unsignName.Replace(" ", "-").ToLower();
+            return unsignName;
         }
     }
 }
